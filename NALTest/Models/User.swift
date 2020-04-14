423f6724374413a89ea8d678a3c681786a94c280
Le Hong Van <hongvan.571996@gmail.com>
2020-04-14 20:22:08 +0700

    Apply object mapper

--- NALTest/Models/User.swift
+++ NALTest/Models/User.swift
@@ -7,8 +7,10 @@
 //
 
 import Foundation
+import ObjectMapper
 
-final class User {
+final class User: Mappable {
+
     var id: Int = 0
     var avatarUrl: String = ""
     var login: String = ""
@@ -19,4 +21,19 @@ final class User {
     var numberOfRepos: Int = 0
     var numberOfFollowers: Int = 0
     var numberOfFollowing: Int = 0
+
+    init?(map: Map) { }
+    
+    func mapping(map: Map) {
+        id <- map["id"]
+        avatarUrl <- map["avatar_url"]
+        login <- map["login"]
+        username <- map["name"]
+        location <- map["location"]
+        bio <- map["bio"]
+        url <- map["html_url"]
+        numberOfRepos <- map["public_repos"]
+        numberOfFollowers <- map["followers"]
+        numberOfFollowing <- map["following"]
+    }
 }
