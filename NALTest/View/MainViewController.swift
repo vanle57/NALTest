b9f4fb8c097c10a0883d7203be76791be5ddbdbc
Le Hong Van <hongvan.571996@gmail.com>
2020-04-14 09:35:18 +0700

    Initialize project

---
+++ NALTest/View/MainViewController.swift
@@ -0,0 +1,9 @@
+//
+//  MainViewController.swift
+//  NALTest
+//
+//  Created by Van Le H. on 4/14/20.
+//  Copyright © 2020 Van Le H All rights reserved.
+//
+
+import Foundation
---
+++ NALTest/View/ProfileDetail/ProfileDetailViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ProfileDetailViewController.swift
+//  NALTest
+//
+//  Created by Van Le H. on 4/14/20.
+//  Copyright © 2020 Van Le H All rights reserved.
+//
+
+import UIKit
+
+class ProfileDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
